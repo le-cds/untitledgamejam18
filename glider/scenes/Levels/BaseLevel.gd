b5c75e6c9d89c9d337bb394b730b609de214c448
Christoph Daniel Schulze <cds@informatik.uni-kiel.de>
2020-03-01 19:09:16 +0100

    Add ability to start levels standalone.

--- glider/scenes/Levels/BaseLevel.gd
+++ glider/scenes/Levels/BaseLevel.gd
@@ -4,6 +4,9 @@ extends State
 func _ready() -> void:
     $StateMachine/PlayingState.aircraft = $Aircraft
 
+    if self.get_parent() == get_tree().root:
+        $StateMachine.transition_push(Constants.GAME_STATE_WAITING)
+
 func state_started(prev_state: State, params: Dictionary) -> void:
     .state_started(prev_state, params)
 
