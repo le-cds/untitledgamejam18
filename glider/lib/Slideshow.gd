8c38398bf6dedc1c704b5989dfaf53f0e1c681d0
Christoph Daniel Schulze <cds@informatik.uni-kiel.de>
2020-03-03 10:12:36 +0100

    Whitespace.

--- glider/lib/Slideshow.gd
+++ glider/lib/Slideshow.gd
@@ -70,29 +70,29 @@ var _tween: Tween
 # Scene Lifecycle
 
 func _init() -> void:
-    _tween = Tween.new()
-    self.add_child(_tween)
+	_tween = Tween.new()
+	self.add_child(_tween)
 
 
 func _ready() -> void:
-    self.modulate = COLOR_TRANSPARENT
+	self.modulate = COLOR_TRANSPARENT
 
 
 func _input(event) -> void:
-    if skippable:
-        # Detect whether the user wants to skip the current slide.
-        var interesting_action := false
+	if skippable:
+		# Detect whether the user wants to skip the current slide.
+		var interesting_action := false
 
-        interesting_action = interesting_action || event.is_action_pressed("ui_accept")
-        interesting_action = interesting_action || event.is_action_pressed("ui_select")
-        interesting_action = interesting_action || event.is_action_pressed("ui_cancel")
+		interesting_action = interesting_action || event.is_action_pressed("ui_accept")
+		interesting_action = interesting_action || event.is_action_pressed("ui_select")
+		interesting_action = interesting_action || event.is_action_pressed("ui_cancel")
 
-        if event is InputEventMouseButton:
-            var button_event := event as InputEventMouseButton
-            interesting_action = interesting_action || button_event.pressed
+		if event is InputEventMouseButton:
+			var button_event := event as InputEventMouseButton
+			interesting_action = interesting_action || button_event.pressed
 
-        if interesting_action:
-            _skip_slide()
+		if interesting_action:
+			_skip_slide()
 
 
 ####################################################################################
@@ -101,108 +101,108 @@ func _input(event) -> void:
 # Starts the slideshow, unless it is already running. Arguments > 0 override the
 # pre time setting and thus wait before showing the first image.
 func start(after: float = -1) -> void:
-    if after > 0:
-        pre_time = after
+	if after > 0:
+		pre_time = after
 
-    if pre_time > 0:
-        yield(get_tree().create_timer(pre_time), "timeout")
+	if pre_time > 0:
+		yield(get_tree().create_timer(pre_time), "timeout")
 
-    if not _running:
-        _curr_texture_index = NO_TEXTURE
-        _running = true
-        _show_next()
+	if not _running:
+		_curr_texture_index = NO_TEXTURE
+		_running = true
+		_show_next()
 
 
 # Shows the next slide.
 func _show_next() -> void:
-    # Don't do anything if there are no textures
-    if textures == null or textures.empty():
-        return
+	# Don't do anything if there are no textures
+	if textures == null or textures.empty():
+		return
 
-    _showing_slide = false
+	_showing_slide = false
 
-    # If we have a current texture, we need to fade that out
-    var had_previous_texture := false
-    if _curr_texture_index != NO_TEXTURE:
-        had_previous_texture = true
+	# If we have a current texture, we need to fade that out
+	var had_previous_texture := false
+	if _curr_texture_index != NO_TEXTURE:
+		had_previous_texture = true
 
-        # Fade out slide, if necessary (if we don't need to fade out, but there
-        # also isn't a next texture, we'll remove the current texture below)
-        if fade_time > 0.0:
-            _tween.interpolate_property(
-                self,
-                "modulate",
-                COLOR_SOLID,
-                COLOR_TRANSPARENT,
-                fade_time,
-                Tween.TRANS_LINEAR,
-                Tween.EASE_IN)
-            _tween.start()
-            yield(_tween, "tween_completed")
+		# Fade out slide, if necessary (if we don't need to fade out, but there
+		# also isn't a next texture, we'll remove the current texture below)
+		if fade_time > 0.0:
+			_tween.interpolate_property(
+				self,
+				"modulate",
+				COLOR_SOLID,
+				COLOR_TRANSPARENT,
+				fade_time,
+				Tween.TRANS_LINEAR,
+				Tween.EASE_IN)
+			_tween.start()
+			yield(_tween, "tween_completed")
 
-    # If we're currently showing the last texture but are set to repeat, we need
-    # to jump back to the first
-    _curr_texture_index += 1
-    if repeat:
-        _curr_texture_index %= textures.size()
+	# If we're currently showing the last texture but are set to repeat, we need
+	# to jump back to the first
+	_curr_texture_index += 1
+	if repeat:
+		_curr_texture_index %= textures.size()
 
-    # If we have a next slide, excellent. Otherwise, we're through!
-    if _curr_texture_index < textures.size():
-        # Wait a while, if necessary
-        if had_previous_texture and black_time > 0.0:
-            yield(get_tree().create_timer(black_time), "timeout")
+	# If we have a next slide, excellent. Otherwise, we're through!
+	if _curr_texture_index < textures.size():
+		# Wait a while, if necessary
+		if had_previous_texture and black_time > 0.0:
+			yield(get_tree().create_timer(black_time), "timeout")
 
-        # Actually set the texture
-        self.texture = textures[_curr_texture_index]
+		# Actually set the texture
+		self.texture = textures[_curr_texture_index]
 
-        # Fade in slide, if necessary
-        if fade_time > 0.0:
-            _tween.interpolate_property(
-                self,
-                "modulate",
-                COLOR_TRANSPARENT,
-                COLOR_SOLID,
-                fade_time,
-                Tween.TRANS_LINEAR,
-                Tween.EASE_OUT)
-            _tween.start()
-            yield(_tween, "tween_completed")
+		# Fade in slide, if necessary
+		if fade_time > 0.0:
+			_tween.interpolate_property(
+				self,
+				"modulate",
+				COLOR_TRANSPARENT,
+				COLOR_SOLID,
+				fade_time,
+				Tween.TRANS_LINEAR,
+				Tween.EASE_OUT)
+			_tween.start()
+			yield(_tween, "tween_completed")
 
-        _showing_slide = true
+		_showing_slide = true
 
-        if _skip_next_slide:
-            # Call this method again immediately
-            _skip_next_slide = false
-            call_deferred("_show_next")
-        else:
-            # Schedule this method to be called again
-            _tween.interpolate_deferred_callback(self, display_time, "_show_next")
-            _tween.start()
+		if _skip_next_slide:
+			# Call this method again immediately
+			_skip_next_slide = false
+			call_deferred("_show_next")
+		else:
+			# Schedule this method to be called again
+			_tween.interpolate_deferred_callback(self, display_time, "_show_next")
+			_tween.start()
 
-    else:
-        # Be sure that we're not displaying anything
-        self.texture = null
+	else:
+		# Be sure that we're not displaying anything
+		self.texture = null
 
-        # There is no next slide; signal the signal!
-        _running = false
-        emit_signal("slideshow_finished")
+		# There is no next slide; signal the signal!
+		_running = false
+		emit_signal("slideshow_finished")
 
 
 # Skips the current slide if one is shown at the moment, or sets a flag to skip
 # the next slide if we're currently transitioning
 func _skip_slide() -> void:
-    if _showing_slide:
-        _tween.stop_all()
-        call_deferred("_show_next")
-    else:
-        _skip_next_slide = true
+	if _showing_slide:
+		_tween.stop_all()
+		call_deferred("_show_next")
+	else:
+		_skip_next_slide = true
 
 
 ####################################################################################
 # Editor Support
 
 func _get_configuration_warning():
-    if textures == null or textures.empty():
-        return "You must configure at least one texture."
+	if textures == null or textures.empty():
+		return "You must configure at least one texture."
 
-    return ""
+	return ""
