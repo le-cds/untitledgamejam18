9e0556045d429fe2e9f333f10dae3da27daf2554
Christoph Daniel Schulze <cds@informatik.uni-kiel.de>
2020-03-03 15:22:27 +0100

    Whitespace again... >:-|

--- glider/scenes/Root/Root.gd
+++ glider/scenes/Root/Root.gd
@@ -16,31 +16,31 @@ var _main_scene_loader: ResourceInteractiveLoader
 # Scene Lifecycle
 
 func _ready() -> void:
-	_slideshow.start()
+    _slideshow.start()
 
-	# Start loading the main scene
-	_main_scene_loader = ResourceLoader.load_interactive("res://scenes/Game.tscn")
+    # Start loading the main scene
+    _main_scene_loader = ResourceLoader.load_interactive("res://scenes/Game.tscn")
 
 
 func _process(delta: float) -> void:
-	# Once our main scene has finished loading, add it to the scene tree so it's
-	# ready when the splash screen disappears.
-	if _main_scene_loader != null and _main_scene_loader.poll() == ERR_FILE_EOF:
-		_main_parent.add_child(_main_scene_loader.get_resource().instance())
+    # Once our main scene has finished loading, add it to the scene tree so it's
+    # ready when the splash screen disappears.
+    if _main_scene_loader != null and _main_scene_loader.poll() == ERR_FILE_EOF:
+        _main_parent.add_child(_main_scene_loader.get_resource().instance())
 
-		# Remove loader and disable processing for this node -- we're done!
-		_main_scene_loader = null
-		set_process(false)
+        # Remove loader and disable processing for this node -- we're done!
+        _main_scene_loader = null
+        set_process(false)
 
 
 ####################################################################################
 # Event Handling
 
 func _on_Slideshow_slideshow_finished() -> void:
-	# Fade out our splash screen and remove it
-	_animation.play("FadeSplash")
-	yield(_animation, "animation_finished")
-	_splash.queue_free()
+    # Fade out our splash screen and remove it
+    _animation.play("FadeSplash")
+    yield(_animation, "animation_finished")
+    _splash.queue_free()
 
-	# This would be dangerous if loading our main menu took long
-	_main_parent.get_child(0).start()
+    # This would be dangerous if loading our main menu took long
+    _main_parent.get_child(0).start()
